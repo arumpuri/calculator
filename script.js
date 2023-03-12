f6c53f490785b3f249e054d6eb2507dbdb1d0480
Arum Puri <arumpuripratamawati@gmail.com>
2023-03-12 23:07:57 +0700

    Edit html file and add script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const calcKeys = document.querySelector('.button-container');
+const userInput = document.querySelector('.user-input');
+const calculator = document.querySelector('.container');
+const displayResult = document.querySelector('#result');
